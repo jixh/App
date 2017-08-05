07fcb1d05c1fff824a4cd7863e23bfa4dcfe5287
jktaihe <zxcvbJKnm12>
2017-08-05 11:00:08 +0800

    Initial commit

---
+++ imagelibrary/src/main/java/com/jktaihe/imagelibrary/ImageManager.java
@@ -0,0 +1,9 @@
+package com.jktaihe.imagelibrary;
+
+/**
+ * Created by jktaihe on 1/8/17.
+ * blog: blog.jktaihe.com
+ */
+
+public class ImageManager {
+}
---
+++ netlibrary/src/main/java/com/jktaihe/netlibrary/NetManager.java
@@ -0,0 +1,10 @@
+package com.jktaihe.netlibrary;
+
+/**
+ * Created by jktaihe on 1/8/17.
+ * blog: blog.jktaihe.com
+ */
+
+public class NetManager {
+    public void init(){}
+}
